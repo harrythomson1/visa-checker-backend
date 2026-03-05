94d192b04fc14b11035af683e8fc857587df025d
Harry Thomson <haroldt95@hotmail.com>
2026-03-05 12:33:10 +0900

    Assert if the database value is empty

--- core/config.py
+++ core/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+assert DATABASE_URL, "DATABASE_URL is not set in .env"
