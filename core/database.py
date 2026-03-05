2b0ef9585620e6318c217ca870eb542f9fb922aa
Harry Thomson <haroldt95@hotmail.com>
2026-03-05 12:37:58 +0900

    Create database connection

---
+++ core/database.py
@@ -0,0 +1,17 @@
+from core.config import DATABASE_URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine(DATABASE_URL)
+
+SessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with SessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
