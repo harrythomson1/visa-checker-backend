eed1a5bd635d3e450cb6ca13938f2ed8a9f6f393
Harry Thomson <haroldt95@hotmail.com>
2026-03-05 11:46:46 +0900

    Add a call to the externam API to get the country details for a specific country

--- services/external/gov_visa_service.py
+++ services/external/gov_visa_service.py
@@ -17,3 +17,15 @@ class GovVisaService:
                 return details
         except (RequestError, HTTPStatusError) as e:
             raise ApiUnavailableError("External API is currently unavailable") from e
+
+    async def get_country_visa_details(self, country_slug):
+        try:
+            async with httpx.AsyncClient(timeout=10.0) as client:
+                url = f"https://www.gov.uk/api/content/foreign-travel-advice/{country_slug}"
+                response = await client.get(url)
+                response.raise_for_status()
+                data = response.json()
+                return data
+        except (RequestError, HTTPStatusError) as e:
+            raise ApiUnavailableError("External API is currently unavailable") from e
+
