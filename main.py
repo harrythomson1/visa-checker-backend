14fb43e965d4d1e10420db0c51e784ea329184ec
Harry Thomson <haroldt95@hotmail.com>
2026-03-06 20:41:07 +0900

    Setup the dtabase connection

---
+++ main.py
@@ -0,0 +1,12 @@
+from core.database import Base, engine
+from models import country
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(lifespan=lifespan)
